766bc37207e5f27efba7ec8f85fc568e143ddfba
Виктор Шмелев < >
2017-09-06 23:36:06 +0300

    I find a solution, but... need more work

--- system/Test3.php
+++ system/Test3.php
@@ -29,43 +29,67 @@ class AcR
 
 class AcQ extends AcR
 {
-	public $fields = [];
+
+	public $where = [];
+
+	public $set = [];
 
 
 	public function __construct()
 	{
+		return $this->builder();
 	}
 
 
-	public function addParams($params)
-	{
-		$this->fields[] = $params;
-		return $this;
-	}
+//	public function addParams($params)
+//	{
+//		$this->fields[] = $params;
+//		return $this;
+//	}
 
 	public function where($param = null)
 	{
-		$this->addParams($param);
+		$this->where[] = $param;
 		return $this;
 	}
 
 	public function set($param)
 	{
-		$this->addParams($param);
+		$this->set[] = $param;
 		return $this;
 	}
 
-
-	public function stringer($from)
+	public function stringer()
 	{
+		$string = sprintf("where: %s, set: %s",
+			implode(", ", $this->where),
+			implode(", ", $this->set)
+		);
 
+		return $string;
 	}
+
+	public function builder()
+	{
+		$bs = $this->stringer();
+		$bs = explode(", ", $bs);
+
+		return $bs;
+	}
+
+	public function __toString()
+	{
+		return $this->stringer();
+	}
+
 }
 
 
 $a = AcR::find()->where(["id" => 1])->set(1);
-//$b = AcR::find()->stringer($a);
+//$b = AcR::find()->stringer();
 
 echo "<pre>";
 print_r($a);
+
 //print_r($b);
+
