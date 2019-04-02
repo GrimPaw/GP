fb399b3e7c742491cbb734bb75bc91987e310b0c
Viktor Shmelev <Whiskasin@yandex.ru>
2019-04-02 17:32:32 +0300

    Return to beginning

--- index.php
+++ index.php
@@ -1,19 +1,2 @@
 <?php
 $loader = require __DIR__ . '/vendor/autoload.php';
-
-$a = new \Engine\Query();
-$a->where('id')->orderBy('name');
-//$aa = $a->create($a);
-
-//$b = new \Engine\QueryBuilder($a);
-//$b->test();
-
-
-
-//$c = new \Engine\Test2();
-//$c->getTest();
-
-echo "<pre>";
-print_r($a);
-//print_r($b);
-//print_r($c);
