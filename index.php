04098a189569804f408de990b181a1ce155e1935
Viktor Shmelev <Whiskasin@yandex.ru>
2019-04-04 14:58:19 +0300

    Некоторые правки

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 $loader = require __DIR__ . '/vendor/autoload.php';
-
+define('DIR_ROOT', dirname(__FILE__));
 $obj = new \Engine\Query();
 
 $a = $obj->select(['id', 'name'])->from('user', 'u')->where('id = 3');
 
+
+$AR = new \Engine\ActiveR();
 //echo  $a;
 
 echo "<pre>";
 print_r($a);
+print_r($AR);
